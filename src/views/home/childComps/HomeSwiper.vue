cd7dae62e9de453fc3e56da2d99ae83446f0579c
zhangfengda <mumubeyond@163.com>
2020-01-14 11:15:58 +0800

    'master'

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,38 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image" alt="" @load='imgLoad'>
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: "HomeSwiper",
+    props: {
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    },
+		methods:{
+			imgLoad:function(){
+				console.log('图片加载成功')
+			}
+		}
+  }
+</script>
+
+<style scoped>
+
+</style>
